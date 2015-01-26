78f9d8aca348e4b88efbacc3fd58e10b4dbe30d8
Gabriel Caruso <gabrielmcaruso@gmail.com>
2015-01-26 00:10:10 -0300

    Proyecto vacio pero compilando

---
+++ features/step_definitions/calcular_liquidacion_steps.rb
@@ -0,0 +1,35 @@
+Given(/^estoy en la pagina de "(.*?)"$/) do |arg1|
+  
+end
+
+Given(/^"(.*?)" esta en la vivienda$/) do |arg1|
+  
+end
+
+Then(/^se muestra el resultado del calculo de liquidacion "(.*?)" "(.*?)" "(.*?)" "(.*?)" "(.*?)" "(.*?)" "(.*?)" "(.*?)" "(.*?)" "(.*?)" "(.*?)" "(.*?)"$/) do |arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11, arg12|
+  
+end
+
+Then(/^se muestra el resultado del calculo de liquidacion con deuda externa "(.*?)" "(.*?)" "(.*?)"$/) do |arg1, arg2, arg3|
+  
+end
+
+Then(/^se muestra el resultado del calculo de liquidacion con deuda interna "(.*?)" "(.*?)" "(.*?)"$/) do |arg1, arg2, arg3|
+  
+end
+
+Then(/^se muestra el resultado del calculo de liquidacion con deuda sin pagos "(.*?)" "(.*?)"$/) do |arg1, arg2|
+  
+end
+
+Given(/^una persona de nombre "(.*?)"$/) do |arg1|
+  
+end
+
+When(/^hago click en "(.*?)"$/) do |arg1|
+  
+end
+
+Then(/^se muestra el mensaje "(.*?)"$/) do |arg1|
+
+end
---
+++ features/step_definitions/registrar_pago_steps.rb
@@ -0,0 +1,7 @@
+Given(/^"(.*?)" registro un pago de "(.*?)"$/) do |arg1, arg2|
+  
+end
+
+Given(/^paga el servicio "(.*?)" con un monto de "(.*?)" el dia "(.*?)"$/) do |arg1, arg2, arg3|
+  
+end
